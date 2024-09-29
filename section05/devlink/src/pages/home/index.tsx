1e8900ccf20760304445bfbb557e0830bf3b97ef
TadeuN1 <tadeunrs@gmail.com>
2024-09-29 19:58:02 -0300

    weekend

--- section05/devlink/src/pages/home/index.tsx
+++ section05/devlink/src/pages/home/index.tsx
@@ -1,3 +1,6 @@
+import { Social } from "../../components/Social"
+import { FaFacebook, FaInstagram, FaYoutube, FaGithub } from "react-icons/fa"
+
 
 export function Home(){
 
@@ -5,7 +8,7 @@ export function Home(){
         <div className="flex flex-col w-full py-4 items-center justify-center" >
             <h1 
             className=" md:text-4xl text-3xl font-bold text-white mt-20">
-            Home</h1>
+            MrNeres.</h1>
             <span className="text-gray-50 mb-5 mt-3"> Veja meus links 👇 </span>
 
             <main className="flex flex-col w-11/12 text-center">
@@ -16,10 +19,32 @@ export function Home(){
                                 Canal no Youtube
                             </p>
                         </a>
+                        
+                    </section>
+
+                    <section className="bg-white mb-4 w-full py-2 rounded-lg select-none
+                    transition-transform hover:scale-105 cursor-pointer">
+                        <a>
+                            <p className="text-base md:text-lg">
+                                Nosso Blog
+                            </p>
+                        </a>
+                        
                     </section>
 
                     <footer className="flex justify-center gap-3 my-4">
-                            
+                            <Social url="/">
+                                <FaFacebook size={35} color="#FFF"/>
+                            </Social>
+                            <Social url="/">
+                                <FaInstagram size={35} color="#FFF"/>
+                            </Social>
+                            <Social url="/">
+                                <FaYoutube size={35} color="#FFF"/>
+                            </Social>
+                            <Social url="/">
+                                <FaGithub size={35} color="#FFF"/>
+                            </Social>
                     </footer>
             </main>
         </div>
